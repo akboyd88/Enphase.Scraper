d327adb84633c9ec19d84ef5c70387fc97962671
akboyd88 <akboyd88@users.noreply.github.com>
2020-12-05 19:54:54 -0600

    compose adjustments

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const EnphaseScraper = require("./EnphaseScraper");
 const Logger = require('./Logger');
 
+const client = require('prom-client');
+
+const gauge = new client.Gauge({ name: 'wattHours', help: 'Instanteous Watt Hours' });
+
+
 const scraper = new EnphaseScraper(process.env.ENPHASE_HOSTNAME, process.env.ENPHASE_SCRAPE_INTERVAL);
 const dataHandler = (data) =>{
     Logger.info("Data received ",data);
